3f6de4848375db5ee14da537c21d6d4296fbcb64
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-22 16:49:28 -0700

    Submitting Python Week 05 - Laboratory # 23

---
+++ word_counter_functions.py
@@ -0,0 +1,12 @@
+# BRYANT RAY M. MANALAD
+# ITELEC2
+# Laboratory # 23 - Group Activity # 01 - Problem 05: Word Counter in a Sentence with Input and Counting Functions
+
+def main():
+    
+    sentence = input("Enter a sentence: ")
+    word_count = len(sentence.split())
+    print(f"The sentence has {word_count} words.")
+
+if __name__ == "__main__":
+    main()
